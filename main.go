1e9c7f414cf4f943216072ef733d8bd95f7a60ba
Robocotik <alexander.starkin@gmail.com>
2023-11-16 19:41:36 +0300

    [ADDED] interface | flag-func converting interface

--- main.go
+++ main.go
@@ -1,23 +1,23 @@
 package main
 
-// -f -s (delete)-> -i (registr) ->
+// -f -s (delete)-> -i (registr) -> --c --d --u
+
 import (
 	"bufio"
 	"flag"
 	"fmt"
-
-	// "io"
+	"io"
 	"os"
 	"strings"
 )
 
 type Flags struct {
+	f int
+	s int
+	i bool
 	c bool
 	d bool
 	u bool
-	i bool
-	f int
-	s int
 }
 
 var flags Flags
@@ -30,7 +30,7 @@ func BoolToInt(el bool) int {
 	}
 }
 
-func CheckI(el bool) {
+func CheckI() {
 	changed_list_count := make(map[string]int)
 	for key, _ := range lines_count {
 		changed_list_count[strings.ToLower(key)]++
@@ -38,30 +38,42 @@ func CheckI(el bool) {
 	for key, _ := range changed_list_count {
 		fmt.Println(key)
 	}
+	fmt.Println("I'M IN I")
 }
 
-func CheckU(el bool) {
+func CheckU() {
 	for key, count := range lines_count {
 		if count == 1 {
 			fmt.Println(key)
 		}
 
 	}
+	fmt.Println("I'M IN U")
 }
 
-func CheckD(el bool) {
+func CheckD() {
 	for key, count := range lines_count {
 		if count > 1 {
 			fmt.Println(key)
 		}
 
 	}
+	fmt.Println("I'M IN D")
 }
 
-func CheckC(el bool) {
+func CheckF() {
+	fmt.Println("I'M IN F")
+}
+
+func CheckS(){
+	fmt.Println("I'M IN S")
+}
+
+func CheckC() {
 	for key, count := range lines_count {
 		fmt.Printf("%d %s\n", count, key)
 	}
+	fmt.Println("I'M IN C")
 }
 
 var lines_count = make(map[string]int)
@@ -90,9 +102,9 @@ func InitialConsoleInput() {
 
 }
 
-func ChechTxtCount(){
-	for _ , el := range flag.Args(){
-		if strings.Contains(el, ".txt"){
+func ChechTxtCount() {
+	for _, el := range flag.Args() {
+		if strings.Contains(el, ".txt") {
 			txtCount++
 		}
 	}
@@ -107,11 +119,18 @@ func CheckForAdditionalInput() {
 	fileInput, err := os.Open(flag.Args()[0])
 	if err != nil {
 		fmt.Println("Error: Occured problem during opening the file")
-		return 
+		return
 	}
-	// data, err := io.ReadAll(fileInput)
-	//fmt.Println(string(data))
-	
+	data, err := io.ReadAll(fileInput)
+	for _, line := range strings.Split(string(data), "\n") {
+		lines_count[line]++
+	}
+	// fmt.Println("------------------")
+	// for key, val := range lines_count{
+	// 	fmt.Println(key, val)
+	// }
+	// fmt.Println("------------------")
+
 	defer fileInput.Close()
 
 	if txtCount == 2 { // check for needing in output.txt
@@ -120,13 +139,28 @@ func CheckForAdditionalInput() {
 		fileOutput, err := os.OpenFile(flag.Args()[1], os.O_WRONLY|os.O_CREATE, 0755)
 		if err != nil {
 			fmt.Println("Error: Occured problem during opening the file")
-			return 
-			
+			return
+
 		}
 		fileOutput.WriteString("WORKS")
 		fileOutput.Close()
 	}
 }
+func CheckFlags() {
+	flagFuncs := map[string]func(){
+		"c": CheckC,
+		"d": CheckD,
+		"i": CheckI,
+		"u": CheckU,
+		"f": CheckF,
+		"s": CheckS,
+	}
+
+	flag.Visit(func(f *flag.Flag) { //theoretically the flag can be false!
+		flagFuncs[f.Name]()
+	})
+}
+
 
 func InitFlags() {
 	// var args = []string{}
@@ -139,25 +173,24 @@ func InitFlags() {
 	flag.BoolVar(&flags.i, "i", false, "to not focus on registr")
 	flag.Parse()
 
+	
 	ChechTxtCount()
-	fmt.Println(flag.Args(), txtCount)
-
-	// flag.Visit(func(f *flag.Flag) {
-	// 	fmt.Println("Flag:", f.Name, "Value:", f.Value)
-	// })
+	// fmt.Println(flag.Args(), txtCount)
 
 }
 
+
 func main() {
 	InitFlags()
-	if txtCount > 0{
+	CheckFlags()
+	if txtCount > 0 {
 		CheckForAdditionalInput()
 	}
 	InitialConsoleInput()
 	// An artificial input source.
 	if BoolToInt(flags.c)+BoolToInt(flags.d)+BoolToInt(flags.u) > 1 {
 		fmt.Println("Error: Incorrect combination of flags (you can't choose more than 1 from --c, --d, --u)")
-		return 
+		return
 
 	}
 
@@ -169,16 +202,16 @@ func main() {
 
 	}
 	if flags.c {
-		CheckC(flags.c)
+		CheckC()
 	}
 	if flags.d {
-		CheckD(flags.d)
+		CheckD()
 	}
 	if flags.u {
-		CheckU(flags.u)
+		CheckU()
 	}
 	if flags.i {
-		CheckI(flags.i)
+		CheckI()
 	}
 
 }
